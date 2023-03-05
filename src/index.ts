8c652f12bfc7b1441ddec8049d0fa3653977a803
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-03-05 15:36:55 -0800

    v1.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,13 @@
-import { marked } from "marked";
-import { JSDOM } from "jsdom";
-import dompurify from "dompurify";
+import compile from "chroma-compiler";
 
-const window = new JSDOM("").window;
-const purify = dompurify(window as unknown as Window);
-
-function parse(src: string): string {
-	const code = [...src.matchAll(/<chroma>([^]*?)<\/chroma>/gi)];
-
-	code.map((chromaReg) => {
-		const chromaString = chromaReg[0];
-
-		while (src.indexOf(chromaString) !== -1) {
-			const noTag = chromaString
-				.replace(/<chroma>/gi, "")
-				.replace(/<\/chroma>/gi, "")
-				.replace(/\t/g, "");
-
-			src = src.replace(
-				chromaString,
-				purify.sanitize(marked(noTag, { async: false }))
-			);
-		}
-	});
-
-	return src;
-}
-
-function chroma() {
+function chroma(tabSize?: number) {
 	return {
 		name: "vite-plugin-svelte-chroma",
 
 		transform(src: string, id: string) {
 			if (/\.ts|\.tsx|\.html|\.svelte|\.js|\.jsx|\.vue/i.test(id)) {
 				return {
-					code: parse(src),
+					code: compile(src, tabSize ? tabSize : 4),
 					map: null,
 				};
 			}
