ff2120b691ff2a258a3b0e6a05a0cd312557e584
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-03-07 22:14:30 -0800

    v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import compile from "chroma-compiler";
+import compile from "@chromajs/chroma-compiler";
 
-function chroma(tabSize?: number) {
+function chroma() {
 	return {
 		name: "vite-plugin-svelte-chroma",
 
 		transform(src: string, id: string) {
 			if (/\.ts|\.tsx|\.html|\.svelte|\.js|\.jsx|\.vue/i.test(id)) {
 				return {
-					code: compile(src, tabSize ? tabSize : 4),
+					code: compile(src),
 					map: null,
 				};
 			}
