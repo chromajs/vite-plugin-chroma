39dc430dc1d3061058e6a4de72b5643bc2a2023d
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-03-07 22:20:16 -0800

    v1.0.2

--- example/viteHTML/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import chroma from "vite-plugin-chroma";
-
-export default defineConfig({
-	plugins: [chroma()],
-});
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import compile from "@chromajs/chroma-compiler";
 
 function chroma() {
 	return {
-		name: "vite-plugin-svelte-chroma",
+		name: "vite-plugin-chroma",
 
 		transform(src: string, id: string) {
 			if (/\.ts|\.tsx|\.html|\.svelte|\.js|\.jsx|\.vue/i.test(id)) {
